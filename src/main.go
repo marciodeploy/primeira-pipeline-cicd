aa1cf2b0a4aa0d65a9c1a0b725cb7d1d8c3a06f7
marciodeploy <marciodeploy@hardcloud.com.br>
2024-09-24 16:13:47 -0300

    nome do pod

--- src/main.go
+++ src/main.go
@@ -12,6 +12,40 @@ func index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Aplicacao exemplo - HARDCLOUD - V4.4.0")
 }
 
+
+
+
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	// Obtém o nome do pod da variável de ambiente
+	podName := os.Getenv("POD_NAME")
+	if podName == "" {
+		podName = "Nome do Pod não encontrado"
+	}
+
+	// Exibe o nome do pod na resposta HTTP
+	fmt.Fprintf(w, "<h1>Nome do Pod: %s</h1>", podName)
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	port := ":8080"
+	fmt.Printf("Servidor rodando na porta %s\n", port)
+	http.ListenAndServe(port, nil)
+}
+
+
+
+
+
+
 func main() {
 	http.HandleFunc("/", index)
 	log.Fatal(http.ListenAndServe("0.0.0.0:5000", nil))
