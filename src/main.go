53b4b682ca9afb66d8e02769911f555f298780be
marciodeploy <marciodeploy@hardcloud.com.br>
2024-09-24 16:37:00 -0300

    RollBack

--- src/main.go
+++ src/main.go
@@ -13,6 +13,18 @@ func index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Aplicacao exemplo - HARDCLOUD - V4.5.0")
 }
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	// Obtém o nome do pod da variável de ambiente
+	podName := os.Getenv("POD_NAME")
+	if podName == "" {
+		podName = "Nome do Pod não encontrado"
+	}
+
+	// Exibe o nome do pod na resposta HTTP
+	fmt.Fprintf(w, "<h1>Nome do Pod: %s</h1>", podName)
+}
+
+
 func main() {
 	http.HandleFunc("/", index)
 	log.Fatal(http.ListenAndServe("0.0.0.0:5000", nil))
